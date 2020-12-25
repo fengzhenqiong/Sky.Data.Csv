ce70b2b5fca73d22125e9f10ccfd3815592c6b63
Sky Feng <im.sky@foxmail.com>
2020-12-25 18:42:43 +0800

    update binary version

--- Sky.Data.Csv.Test/Properties/AssemblyInfo.cs
+++ Sky.Data.Csv.Test/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sky.Data.Csv.Test")]
-[assembly: AssemblyDescription("A simple and fast comma based CSV reader and writer")]
+[assembly: AssemblyDescription("A simple and extremely fast CSV reader and writer")]
 [assembly: AssemblyProduct("Sky.Data.Csv.Test")]
 [assembly: AssemblyCompany("SkySoft, Inc.")]
-[assembly: AssemblyCopyright("Copyright © SkySoft 2020")]
+[assembly: AssemblyCopyright("Copyright © SkySoft 2021")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
